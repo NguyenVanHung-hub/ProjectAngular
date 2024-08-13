596c962fbed92df9c1238bd15a3369eb73f8d906
NguyenVanHung-hub <115867880+NguyenVanHung-hub@users.noreply.github.com>
2024-08-13 14:01:42 +0700

    update

--- src/app/component-adim/home-adminconten/home-adminconten.component.ts
+++ src/app/component-adim/home-adminconten/home-adminconten.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HomeAdmimComponent } from "../../page-adim/home-admim/home-admim.component";
+
 import { MenuAdminComponent } from "../menu-admin/menu-admin.component";
 import { FooterAdminComponent } from "../footer-admin/footer-admin.component";
 
 @Component({
   selector: 'app-home-adminconten',
   standalone: true,
-  imports: [HomeAdmimComponent, MenuAdminComponent, FooterAdminComponent],
+  imports: [ MenuAdminComponent, FooterAdminComponent],
   templateUrl: './home-adminconten.component.html',
   styleUrl: './home-adminconten.component.css'
 })
